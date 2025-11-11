7ade36f8ce1f0916753de1fc362bc8bc7aec2190
Hadar Barel <hadarbarel144@gmail.com>
2025-11-11 20:26:22 +0200

    hw2

--- Cheers.java
+++ Cheers.java
@@ -5,25 +5,25 @@
 public class Cheers {
         public static void main(String[] args) {
              
-             String str = args [0];
+             String original_String = args [0];
 	     int n = Integer.parseInt(args[1]);
-             
-             String need_an = "A a E e F f H h I i L l M  m N n O o R r S s X x";
+             String need_an = "A E F H I L M N O R S X";
+             String new_String = original_String.toUpperCase();   
 
-             for (int i = 0; i < str.length(); i ++){
+             for (int i = 0; i < new_String.length(); i ++){
 
-                if (need_an.indexOf(str.charAt(i)) == -1){
-                        System.out.println("Give me " + "a  "+ str.charAt(i) + ": "+ str.charAt(i) + "!");
+                if (need_an.indexOf(new_String.charAt(i)) == -1){
+                        System.out.println("Give me " + "a  "+ new_String.charAt(i) + ": "+ new_String.charAt(i) + "!");
                 }
                 else {
-                        System.out.println("Give me " + "an "+ str.charAt(i) + ": "+ str.charAt(i) + "!");
+                        System.out.println("Give me " + "an "+ new_String.charAt(i) + ": "+ new_String.charAt(i) + "!");
                 }
              }
 
               System.out.println("What does that spell?");
 
                 for (int k = 0; k < n; k ++){
-                        System.out.println(str + "!!!");
+                        System.out.println(new_String + "!!!");
                 }
 
         }
